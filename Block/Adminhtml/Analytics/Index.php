2f0e4ab7a31d2b28db054e636106598a1e8c8d48
Betty Suravech <bsuravech@gmail.com>
2018-10-24 19:32:03 -0700

    working out index block for store switcher

--- Block/Adminhtml/Analytics/Index.php
+++ Block/Adminhtml/Analytics/Index.php
@@ -2,17 +2,88 @@
 
 namespace Algolia\AlgoliaSearch\Block\Adminhtml\Analytics;
 
-use Magento\Backend\Block\Widget\Form\Container;
+use Algolia\AlgoliaSearch\Helper\AlgoliaHelper;
+use Algolia\AlgoliaSearch\Helper\ConfigHelper;
+use Algolia\AlgoliaSearch\Helper\AnalyticsHelper;
+use Algolia\AlgoliaSearch\Helper\Data;
+use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 
-class Index extends Container
+class Index extends Template
 {
-    protected function _construct()
-    {
-        $this->_objectId = 'id';
-        $this->_blockGroup = 'Algolia_AlgoliaSearch';
-        $this->_controller = 'adminhtml_analytics';
+    /** @var DateTime */
+    private $dateTime;
 
-        parent::_construct();
-        $this->buttonList->update('save', 'label', __('Update Store'));
+    /** @var AlgoliaHelper */
+    private $algoliaHelper;
+
+    /** @var ObjectManagerInterface */
+    private $objectManger;
+
+    /** @var ConfigHelper */
+    private $configHelper;
+
+    /** @var AnalyticsHelper */
+    private $analyticsHelper;
+
+    /** @var Data */
+    private $dataHelper;
+
+    /** @var Product */
+    private $productHelper;
+
+    /**
+     * @param Context $context
+     * @param DateTime $dateTime
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        AlgoliaHelper $algoliaHelper,
+        DateTime $dateTime,
+        ObjectManagerInterface $objectManager,
+        ConfigHelper $configHelper,
+        AnalyticsHelper $analyticsHelper,
+        ProductHelper $productHelper,
+        Data $dataHelper,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->dateTime = $dateTime;
+        $this->algoliaHelper = $algoliaHelper;
+        $this->objectManger = $objectManager;
+        $this->configHelper = $configHelper;
+        $this->dataHelper = $dataHelper;
+        $this->productHelper = $productHelper;
+        $this->analyticsHelper = $analyticsHelper;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndexName()
+    {
+        return $this->dataHelper->getIndexName($this->productHelper->getIndexNameSuffix(), $this->getStore()->getId());
+    }
+
+    public function getTopSearches()
+    {
+        return $this->analyticsHelper->getTopSearches(['index' => $this->getIndexName()]);
+    }
+
+    /**
+     * @return Magento\Store\Model\StoreManagerInterface
+     */
+    public function getStore()
+    {
+        $storeManager = $this->objectManger->get('Magento\Store\Model\StoreManagerInterface');
+        if ($storeId = $this->getRequest()->getParam('store')) {
+            return $storeManager->getStore($storeId);
+        }
+
+        return $storeManager->getDefaultStoreView();
     }
 }
