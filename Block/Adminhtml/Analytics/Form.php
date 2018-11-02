beaf074290dae27fc37d6d777a49794faa3116fc
Betty Suravech <bsuravech@gmail.com>
2018-11-02 12:33:15 -0700

    daily search table analytics

--- Block/Adminhtml/Analytics/Form.php
+++ Block/Adminhtml/Analytics/Form.php
@@ -2,28 +2,8 @@
 
 namespace Algolia\AlgoliaSearch\Block\Adminhtml\Analytics;
 
-use Magento\Backend\Block\Template;
-use Magento\Backend\Block\Template\Context;
-
-class DateRange extends Template
+class Form extends Index
 {
-    /**
-     * DateRange constructor.
-     * @param Context $context
-     * @param array $data
-     */
-    public function __construct(
-        Context $context,
-        array $data = []
-    ) {
-        parent::__construct(
-            $context,
-            $data
-        );
-
-        $this->setTemplate('analytics/daterange.phtml');
-    }
-
     /**
      * @return string
      */
--- Block/Adminhtml/Analytics/Index.php
+++ Block/Adminhtml/Analytics/Index.php
@@ -7,6 +7,8 @@ use Algolia\AlgoliaSearch\Helper\ConfigHelper;
 use Algolia\AlgoliaSearch\Helper\AnalyticsHelper;
 use Algolia\AlgoliaSearch\Helper\Data;
 use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
+use Algolia\AlgoliaSearch\Helper\Entity\CategoryHelper;
+use Algolia\AlgoliaSearch\Helper\Entity\PageHelper;
 use Magento\Backend\Block\Template;
 use Magento\Backend\Block\Template\Context;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
@@ -14,6 +16,7 @@ use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 class Index extends Template
 {
     const LIMIT_RESULTS = 5;
+    const DEFAULT_TYPE = 'products';
 
     /** @var Context */
     private $backendContext;
@@ -33,13 +36,21 @@ class Index extends Template
     /** @var Product */
     private $productHelper;
 
+    /** @var CategoryHelper */
+    protected $categoryHelper;
+
+    /** @var PageHelper */
+    protected $pageHelper;
+
     /** @var TimezoneInterface */
     private $dateTime;
 
-    /** @var array  */
     protected $_analyticsParams = array();
 
-    protected $_totalSearches;
+    /** Cache variables to prevent excessive calls */
+    protected $_searches;
+    protected $_users;
+    protected $_rateOfNoResults;
 
     /**
      * Index constructor.
@@ -48,6 +59,8 @@ class Index extends Template
      * @param ConfigHelper $configHelper
      * @param AnalyticsHelper $analyticsHelper
      * @param ProductHelper $productHelper
+     * @param CategoryHelper $categoryHelper
+     * @param PageHelper $pageHelper
      * @param Data $dataHelper
      * @param TimezoneInterface $dateTime
      * @param array $data
@@ -58,6 +71,8 @@ class Index extends Template
         ConfigHelper $configHelper,
         AnalyticsHelper $analyticsHelper,
         ProductHelper $productHelper,
+        CategoryHelper $categoryHelper,
+        PageHelper $pageHelper,
         Data $dataHelper,
         TimezoneInterface $dateTime,
         array $data = []
@@ -69,17 +84,20 @@ class Index extends Template
         $this->configHelper = $configHelper;
         $this->dataHelper = $dataHelper;
         $this->productHelper = $productHelper;
+        $this->categoryHelper = $categoryHelper;
+        $this->pageHelper = $pageHelper;
         $this->analyticsHelper = $analyticsHelper;
         $this->dateTime = $dateTime;
     }
 
     /**
-     * @return string
+     * @return mixed
      * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function getIndexName()
     {
-        return $this->dataHelper->getIndexName($this->productHelper->getIndexNameSuffix(), $this->getStore()->getId());
+        $sections = $this->getSections();
+        return $sections[$this->getCurrentType()];
     }
 
     /**
@@ -109,42 +127,130 @@ class Index extends Template
         return array_merge($this->_analyticsParams, $additional);
     }
 
-    public function getTotalSearches()
+    public function getSearches()
     {
-        if (!$this->_totalSearches) {
-            $this->_totalSearches = $this->analyticsHelper->getCountOfSearches($this->getAnalyticsParams());
+        if (!$this->_searches) {
+            $this->_searches = $this->analyticsHelper->getCountOfSearches($this->getAnalyticsParams());
         }
-        return $this->_totalSearches;
+        return $this->_searches;
     }
 
-    public function getDateCounts()
+    public function getTotalCountOfSearches()
     {
-        $total = $this->getTotalSearches();
-        return $total && isset($total['dates']) ? $total['dates'] : array();
+        $searches = $this->getSearches();
+        return $searches && isset($searches['count']) ? $searches['count'] : 0;
     }
 
-    public function getTotalCount()
+    public function getSearchesByDates()
     {
-        $total = $this->getTotalSearches();
-        return $total && isset($total['count']) ? $total['count'] : 0;
+        $searches = $this->getSearches();
+        return $searches && isset($searches['dates']) ? $searches['dates'] : array();
     }
 
     public function getUsers()
     {
-        $analytics = $this->analyticsHelper->getUserCount($this->getAnalyticsParams());
-        return isset($analytics['count']) ? $analytics['count'] : 0;
+        if (!$this->_users) {
+            $this->_users = $this->analyticsHelper->getUserCount($this->getAnalyticsParams());
+        }
+        return $this->_users;
     }
 
-    public function getResultRate()
+    public function getTotalUsersCount()
     {
-        $analytics = $this->analyticsHelper->getRateOfNoResults($this->getAnalyticsParams());
-        return isset($analytics['rate']) ? round($analytics['rate'] * 100) . '%' : 0;
+        $users = $this->getUsers();
+        return $users && isset($users['count']) ? $users['count'] : 0;
     }
+
+    public function getUsersCountByDates()
+    {
+        $users = $this->getUsers();
+        return $users && isset($users['dates']) ? $users['dates'] : array();
+    }
+
+    public function getRateOfNoResults()
+    {
+        if (!$this->_rateOfNoResults) {
+            $this->_rateOfNoResults = $this->analyticsHelper->getRateOfNoResults($this->getAnalyticsParams());
+        }
+        return $this->_rateOfNoResults;
+    }
+
+    public function getTotalResultRates()
+    {
+        $result = $this->getRateOfNoResults();
+        return $result && isset($result['rate']) ? round($result['rate'] * 100, 2) . '%' : 0;
+    }
+
+    public function getResultRateByDates()
+    {
+        $result = $this->getRateOfNoResults();
+        return $result && isset($result['dates']) ? $result['dates'] : array();
+    }
+
+    public function getDailySearchData()
+    {
+        $searches = $this->getSearchesByDates();
+        $users = $this->getUsersCountByDates();
+        $rates = $this->getResultRateByDates();
+
+        foreach ($searches as &$search) {
+            $search['users'] = $this->getDateValue($users, $search['date'], 'count');
+            $search['rate'] = $this->getDateValue($rates, $search['date'], 'rate');
+        }
+
+        return $searches;
+    }
+
+    /**
+     * @param $array
+     * @param $date
+     * @param $valueKey
+     * @return string
+     */
+    protected function getDateValue($array, $date, $valueKey)
+    {
+        $value = '';
+        foreach ($array as $item) {
+            if ($item['date'] === $date) {
+                $value = $item[$valueKey];
+                if ($valueKey == 'rate') {
+                    $value = round($value * 100, 2) . '%';
+                }
+                break;
+            }
+        }
+        return $value;
+    }
+
 
     public function getTopSearches()
     {
         $topSearches = $this->analyticsHelper->getTopSearches($this->getAnalyticsParams(array('limit' => self::LIMIT_RESULTS)));
         return isset($topSearches['searches']) ? $topSearches['searches'] : array();
+    }
+
+    public function getPopularResults()
+    {
+        $popular = $this->analyticsHelper->getTopHits($this->getAnalyticsParams(array('limit' => self::LIMIT_RESULTS)));
+        $hits = isset($popular['hits']) ? $popular['hits'] : array();
+
+        // Maybe pull Magento product Collection? This seems to be an expensive call
+        /* if (count($hits)) {
+            $objectIds = array_map(function($arr) {
+                return $arr['hit'];
+            }, $hits);
+
+            $objects = $this->algoliaHelper->getObjects($this->getIndexName(), $objectIds);
+            foreach ($hits as &$hit) {
+                foreach ($objects['results'] as $object) {
+                    if ($object['objectID'] == $hit['hit']) {
+                        $hit['object'] = $object;
+                    }
+                }
+            }
+        } */
+
+        return $hits;
     }
 
     public function getNoResultSearches()
@@ -167,6 +273,40 @@ class Index extends Template
         }
 
         return true;
+    }
+
+    public function getCurrentType()
+    {
+        if ($formData = $this->_backendSession->getAlgoliaAnalyticsFormData()) {
+            if (isset($formData['type'])) {
+                return $formData['type'];
+            }
+        }
+        return self::DEFAULT_TYPE;
+    }
+
+    /**
+     * @return array
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getSections()
+    {
+        return $sections = array(
+            'products' => $this->dataHelper->getIndexName($this->productHelper->getIndexNameSuffix(), $this->getStore()->getId()),
+            // 'categories' => $this->dataHelper->getIndexName($this->categoryHelper->getIndexNameSuffix(), $this->getStore()->getId()),
+            'pages' => $this->dataHelper->getIndexName($this->pageHelper->getIndexNameSuffix(), $this->getStore()->getId())
+        );
+    }
+
+    public function getTypeEditUrl($objectId)
+    {
+        if ($this->getCurrentType() == 'products') {
+            return $this->getUrl('catalog/product/edit', array('id' => $objectId));
+        }
+
+        if ($this->getCurrentType() == 'pages') {
+            return $this->getUrl('cms/page/edit', array('page_id' => $objectId));
+        }
     }
 
     /**
